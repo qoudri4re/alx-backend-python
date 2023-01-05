0488d7f645aaa59a2b50a4824b7a522fafb6ba0e
qoudri4re <qoudri4re@gmail.com>
2023-01-05 22:51:01 +0100

    mandatory tasks

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""
-contains a function  takes a string k 
-and an int OR float v as 
-arguments and returns a tuple.
-"""
-
-from typing import List, Tuple, Union
+'''Task 7's module.
+'''
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''logic'''
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
+    '''
     return (k, float(v**2))
